4371e58a73ff4bbfd4f94cbbebdd9db0c508d4b0
AprBreeze <immarkzhou@gmail.com>
2025-05-10 19:24:54 -0400

    CharacterMovement

---
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using System.Collections; // Required for IEnumerator
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement2D : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public float jumpDelay = 0.2f;
+    
+    [Header("State")]
+    [SerializeField] private bool _isGrounded;
+    public bool IsGrounded { get => _isGrounded; private set => _isGrounded = value; }
+    
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector3 originalScale;
+    private bool isFacingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        originalScale = transform.localScale;
+    }
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+    
+    void Update()
+    {
+        HandleJump();
+        UpdateAnimationState();
+    }
+    
+    void HandleMovement()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        
+        animator.SetFloat("moveSpeed", Mathf.Abs(moveInput));
+        
+        // Flip character based on movement direction
+        if (moveInput > 0 && !isFacingRight || moveInput < 0 && isFacingRight)
+            Flip();
+    }
+    
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && IsGrounded)
+        {
+            IsGrounded = false; // prevent double jumps
+            animator.SetBool("IsJumping", true);
+            StartCoroutine(ApplyJumpForceAfterDelay(jumpDelay));
+        }
+    }
+    
+    void UpdateAnimationState()
+    {
+        // Jumping/Falling animation states
+        if (!IsGrounded)
+        {
+            if (rb.linearVelocity.y > 0.1f)
+            {
+                animator.SetBool("IsJumping", true);
+                animator.SetBool("IsFalling", false);
+            }
+            else if (rb.linearVelocity.y < -0.1f)
+            {
+                animator.SetBool("IsJumping", false);
+                animator.SetBool("IsFalling", true);
+            }
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+        }
+    }
+    
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            IsGrounded = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        IsGrounded = false;
+    }
+
+    private IEnumerator ApplyJumpForceAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce*3);
+    }
+}
