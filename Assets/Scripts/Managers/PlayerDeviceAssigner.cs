b9c48a98f9c66088d5bcf35cd93333a07524b50e
Alan Fan <fwt2584190223@gmail.com>
2025-05-12 17:55:10 -0400

    better input system

---
+++ Assets/Scripts/Managers/PlayerDeviceAssigner.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+
+public class PlayerDeviceAssigner : MonoBehaviour
+{
+    public PlayerInput player1;
+    public PlayerInput player2;
+
+    void Start()
+    {
+        
+        AssignDevices();
+    }
+
+    void AssignDevices()
+    {
+        var gamepads = Gamepad.all;
+        var keyboard = Keyboard.current;
+
+        if (gamepads.Count >= 2)
+        {
+            Debug.Log("Two gamepads detected");
+            // Two gamepads
+            PairDevice(player1, gamepads[0]);
+            PairDevice(player2, gamepads[1]);
+        }
+        else if (gamepads.Count == 1)
+        {
+            // One gamepad and one keyboard
+            PairDevice(player1, keyboard);
+            PairDevice(player2, gamepads[0]);
+        }
+        else
+        {
+            // Two players share one keyboard
+            PairDevice(player1, keyboard);
+            PairDevice(player2, keyboard);
+        }
+    }
+
+    void PairDevice(PlayerInput player, InputDevice device)
+    {
+        if (player == null || device == null) return;
+
+        // Detach any existing device bindings
+        player.user.UnpairDevices();
+        
+        // Pair the new device to the user
+        InputUser.PerformPairingWithDevice(device, player.user);
+
+        // Make sure the user is active
+        player.user.ActivateControlScheme(player.defaultControlScheme);
+
+        Debug.Log($"Assigned {device.displayName} to {player.gameObject.name}");
+    }
+}
